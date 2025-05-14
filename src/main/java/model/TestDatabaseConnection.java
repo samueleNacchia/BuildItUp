a92409a393e62d38ca9e224532a72a1a52bd0f63
samueleNacchia <samu.nacchia@gmail.com>
2025-05-14 12:33:13 +0200

    Aggiunta TestDatabaseConnection

---
+++ src/main/java/model/TestDatabaseConnection.java
@@ -0,0 +1,22 @@
+package model;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestDatabaseConnection {
+
+    public static void main(String[] args) {
+    	DataSource dataSource = new MysqlDataSource();
+        ((MysqlDataSource) dataSource).setURL("jdbc:mysql://localhost:3306/storage");
+        ((MysqlDataSource) dataSource).setUser("user");
+        ((MysqlDataSource) dataSource).setPassword("12345678");
+
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println("Connessione al database riuscita!");
+        } catch (SQLException e) {
+            System.out.println("Errore nella connessione al database: " + e.getMessage());
+        }
+    }
+}
